0c81e4db2a36a919e25b5cdc29a57009ea86ae51
Anuj <dagadeanuj@gmail.com>
2023-02-19 15:06:26 +0000

    BoilerPlate

--- sem2/banker.c
+++ sem2/banker.c
@@ -1,1 +1,27 @@
+#include <stdio.h>
 
+
+int Available[4];
+int Total[4] = {6,5,7,6};
+int Max[3][4] = {{3,3,2,2}, {1,2,3,4}, {1,3,5,0}};
+int Need[3][4] = {};
+int Allocated[3][4] = {{1,2,2,1}, {1,0,3,3}, {1,2,1,0}};
+
+void calculateNeed()
+{
+
+}
+
+void calculateAvailable()
+{
+
+}
+
+
+int main()
+{
+
+
+
+    return 0;
+}
